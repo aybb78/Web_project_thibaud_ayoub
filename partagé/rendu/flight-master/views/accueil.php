a5415cd7cb61b43acb8240712f03149e968cc879
aybb78 <130739394+aybb78@users.noreply.github.com>
2023-11-26 17:35:54 +0100

    acceuil retravaillé

--- partagé/rendu/flight-master/views/accueil.php
+++ partagé/rendu/flight-master/views/accueil.php
@@ -4,19 +4,34 @@
 
     <link rel="stylesheet" href="assets/style.css">
 </head>
-<body>
-
-    <div id="title" class=texte>Chrono</div>
-    
-    <div id="son">   
-    <button id="speakerButton" onclick="toggleAudio()">🔊</button>
-    <audio id="myAudio" loop>
-        <source src="assets/audio/intro.m4a" type="audio/mp3">
-        Votre navigateur ne supporte pas l'élément audio.
-    </audio>
+<body id="body-acceuil">
+    <div id="title1" class=texte>
+        Master's Treasure
     </div>
     
+    <div id="son">   
+        <button id="speakerButton" onclick="toggleAudio()">🔊</button>
+        <audio id="myAudio" loop>
+            <source src="assets/audio/intro.m4a" type="audio/mp3">
+            Votre navigateur ne supporte pas l'élément audio.
+        </audio>
+    </div>
 
+    <div id="contenu">
+        <form action="index.php" method="get">
+            <label id= "for" for="name">Votre pseudo :</label>
+            <input type="text" name="pseudo" required>
+            <br>
+            <label id= "for" for="prof">Votre mentor :</label>
+            <select name="Mentor" required>
+                <option value="" disabled selected>Choisissez un mentor</option>
+                <option value="1">Victor</option>
+                <option value="2">Vincent</option>
+            </select>
+            <br>
+            <button type="submit">Commencer</button>
+        </form>
+    </div>
 
     <script>
         var audio = document.getElementById("myAudio");
